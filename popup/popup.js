6f59794f0e5a279e3d5cef26a77afcefd1e0378c
Holmes Zhi <holmeszhi@qq.com>
2025-10-27 22:50:19 +0800

    fix: 重构以符合 edge 发布结构

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,12 @@
 // Popup script
 
 const btnOptions = document.getElementById('btnOptions');
+const versionText = document.getElementById('version');
 
 btnOptions.addEventListener('click', () => {
     chrome.runtime.openOptionsPage();
 });
+
+// 显示当前版本号
+const manifest = chrome.runtime.getManifest();
+versionText.textContent = `v${manifest.version}`;
